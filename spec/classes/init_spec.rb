c94dfe3d2208aa94df1dc547038a6e60dc460741
Sergio Jimenez <tripledes@gmail.com>
2014-09-01 00:30:11 +0200

    Initial tests

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,18 @@ require 'spec_helper'
 describe 'lxc' do
 
   context 'with defaults for all parameters' do
+
+    let(:facts) { {:operatingsystem => 'Ubuntu', :operatingsystemrelease => '14.04'} }
+
     it { should contain_class('lxc') }
+
+    it { should contain_package('build-essential','ruby-dev','lxc-dev').with_ensure('latest') }
+
+    it { should contain_package('lxc-bindings').with(
+      {
+        :name     => 'ruby-lxc',
+        :provider => :gem,
+      })
+    }
   end
 end
