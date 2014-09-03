0671518c5d9705b2d11a6837c70c9c657aa7039f
Sergio Jimenez <tripledes@gmail.com>
2014-09-04 00:21:51 +0200

    type tested

--- spec/unit/type/lxc_spec.rb
+++ spec/unit/type/lxc_spec.rb
@@ -32,5 +32,37 @@ describe Puppet::Type.type(:lxc), 'when validating attribute values' do
     Puppet::Type.type(:lxc).new(:name => 'lol_container', :state => :frozen)
   end
 
+  it 'should raise error for :nonsense as value for :storage_backend' do
+    expect { Puppet::Type.type(:lxc).new(:name => 'lol_container', :state => :running, :storage_backend => :nonsense) }.to raise_error
+  end
+
+  [:dir, :lvm, :btrfs, :loop, :best].each do |backend|
+    it "should support #{backend} as value for :storage_backend" do
+      expect { Puppet::Type.type(:lxc).new(:name => 'lol_container', :state => :running, :storage_backend => backend) }.to_not raise_error
+    end
+  end
+
+  it 'should raise error for non-Hash type as value for :storage_options' do
+    expect { Puppet::Type.type(:lxc).new(:name => 'lol_container', :state => :running, :storage_backend => :lvm, :storage_options => [0,1]) }.to raise_error
+  end
+
+  it 'should raise error for invalid key on :storage_options Hash' do
+    expect { Puppet::Type.type(:lxc).new(:name => 'lol_container', :state => :running, :storage_backend => :lvm, :storage_options => {'invalid_key' => 1 }) }.to raise_error
+  end
+
+  it 'should support dir as valid key for :storage_options Hash' do
+    expect { Puppet::Type.type(:lxc).new(:name => 'lol_container', :state => :running, :storage_backend => :lvm, :storage_options => {'dir' => '/tmp' }) }.to_not raise_error
+  end
+
+  it 'should support vgname, lvname, thinpool, fstype and fssize as valid keys for :storage_options Hash' do
+    storage_options = {
+      'vgname' => 'lxc',
+      'lvname' => 'lol_container01',
+      'thinpool' => 'oversized01',
+      'fstype' => 'xfs',
+      'fssize' => '2048'
+    }
+    expect { Puppet::Type.type(:lxc).new(:name => 'lol_container', :state => :running, :storage_backend => :lvm, :storage_options => storage_options) }.to_not raise_error
+  end
 end
 
