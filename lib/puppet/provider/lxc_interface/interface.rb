da5b442835d6dd8aa7fc4c9fb3ff4e2947175b42
Sergio Jimenez <tripledes@gmail.com>
2014-09-20 00:26:18 +0200

    #create and its tests for lxc_interface

--- lib/puppet/provider/lxc_interface/interface.rb
+++ lib/puppet/provider/lxc_interface/interface.rb
@@ -6,11 +6,26 @@ Puppet::Type.type(:lxc_interface).provide(:interface) do
   attr_accessor :container
 
   def create
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.name", @resource[:name])
+      @container.set_config_item("lxc.network.#{@resource[:index]}.flags", @resource[:flags])
+      @container.set_config_item("lxc.network.#{@resource[:index]}.type", @resource[:type])
+      @container.set_config_item("lxc.network.#{@resource[:index]}.link", @resource[:link]) unless @resource[:link].nil?
+      @container.set_config_item("lxc.network.#{@resource[:index]}.vlan_id", @resource[:vlan_id]) unless @resource[:vlan_id].nil?
+      @container.set_config_item("lxc.network.#{@resource[:index]}.macvlan_mode", @resource[:macvlan_mode]) unless @resource[:macvlan_mode].nil?
+      @container.set_config_item("lxc.network.#{@resource[:index]}.ipv4", @resource[:ipv4]) unless @resource[:ipv4].nil?
+      @container.set_config_item("lxc.network.#{@resource[:index]}.hwaddr", @resource[:hwaddr]) unless @resource[:hwaddr].nil?
+      @container.save_config
+      true
+    rescue LXC::Error
+      false
+    end
   end
 
   def exists?
-    define_container
     begin
+      define_container
       @container.keys("lxc.network.#{@resource[:index]}")
       true
     rescue LXC::Error
--- spec/unit/provider/interface_spec.rb
+++ spec/unit/provider/interface_spec.rb
@@ -20,5 +20,20 @@ describe Puppet::Type.type(:lxc_interface).provider(:interface), 'basic interfac
       @provider.container.stubs(:keys).raises(LXC::Error)
       @provider.send(:exists?).should == false
     end
+    it 'should return true when the interface exists' do
+      @provider.container.stubs(:keys).returns(['name','hwaddr','ipv4','ipv6'])
+      @provider.send(:exists?).should == true
+    end
+  end
+  describe '#create' do
+    it 'should return true when settings are applied successfully' do
+      @provider.container.stubs(:save_config)
+      @provider.container.stubs(:set_config_item)
+      @provider.send(:create).should == true
+    end
+    it 'should return false when LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:create).should == false
+    end
   end
 end
