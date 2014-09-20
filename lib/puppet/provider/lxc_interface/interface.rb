0276708956f7f90b0d54e5b37ea11fb89aa713ad
Sergio Jimenez <tripledes@gmail.com>
2014-09-20 13:45:13 +0200

    lxc_interface should be ready for a real test

--- lib/puppet/provider/lxc_interface/interface.rb
+++ lib/puppet/provider/lxc_interface/interface.rb
@@ -34,11 +34,170 @@ Puppet::Type.type(:lxc_interface).provide(:interface) do
   end
 
   def destroy
+    begin
+      define_container
+      @container.keys("lxc.network.#{@resource[:index]}").each do |k|
+        @container.clear_config_item("lxc.network.#{@resource[:index]}.#{k}")
+      end
+      @container.save_config
+      true
+    rescue LXC::Error
+      false
+    end
   end
 
   def define_container
     unless @container
       @container = LXC::Container.new(@resource[:container])
+    end
+  end
+
+  # getters and setters
+
+  def flags
+    begin
+      define_container
+      @container.config_item("lxc.network.#{@resource[:index]}.flags")
+    rescue LXC::Error
+      # TODO: might be better to fail here instead of returning empty string which
+      # would trigger the setter
+      ""
+    end
+  end
+
+  def flags=(value)
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.flags",value)
+      true
+    rescue LXC::Error
+      false
+    end
+  end
+
+  def link
+    begin
+      define_container
+      @container.config_item("lxc.network.#{@resource[:index]}.link")
+    rescue LXC::Error
+      # TODO: might be better to fail here instead of returning empty string which
+      # would trigger the setter
+      ""
+    end
+  end
+
+  def link=(value)
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.link",value)
+      true
+    rescue LXC::Error
+      false
+    end
+  end
+
+  def vlan_id
+    begin
+      define_container
+      @container.config_item("lxc.network.#{@resource[:index]}.vlan_id")
+    rescue LXC::Error
+      # TODO: might be better to fail here instead of returning empty string which
+      # would trigger the setter
+      ""
+    end
+  end
+
+  def vlan_id=(value)
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.vlan_id",value)
+      true
+    rescue LXC::Error
+      false
+    end
+  end
+
+  def macvlan_mode
+    begin
+      define_container
+      @container.config_item("lxc.network.#{@resource[:index]}.macvlan_mode")
+    rescue LXC::Error
+      # TODO: might be better to fail here instead of returning empty string which
+      # would trigger the setter
+      ""
+    end
+  end
+
+  def macvlan_mode=(value)
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.macvlan_mode",value)
+      true
+    rescue LXC::Error
+      false
+    end
+  end
+
+  def type
+    begin
+      define_container
+      @container.config_item("lxc.network.#{@resource[:index]}.type")
+    rescue LXC::Error
+      # TODO: might be better to fail here instead of returning empty string which
+      # would trigger the setter
+      ""
+    end
+  end
+
+  def type=(value)
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.type",value)
+      true
+    rescue LXC::Error
+      false
+    end
+  end
+
+  def ipv4
+    begin
+      define_container
+      @container.config_item("lxc.network.#{@resource[:index]}.ipv4")
+    rescue LXC::Error
+      # TODO: might be better to fail here instead of returning empty string which
+      # would trigger the setter
+      ""
+    end
+  end
+
+  def ipv4=(value)
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.ipv4",value)
+      true
+    rescue LXC::Error
+      false
+    end
+  end
+
+  def hwaddr
+    begin
+      define_container
+      @container.config_item("lxc.network.#{@resource[:index]}.hwaddr")
+    rescue LXC::Error
+      # TODO: might be better to fail here instead of returning empty string which
+      # would trigger the setter
+      ""
+    end
+  end
+
+  def hwaddr=(value)
+    begin
+      define_container
+      @container.set_config_item("lxc.network.#{@resource[:index]}.hwaddr",value)
+      true
+    rescue LXC::Error
+      false
     end
   end
 end
--- spec/unit/provider/interface_spec.rb
+++ spec/unit/provider/interface_spec.rb
@@ -21,10 +21,11 @@ describe Puppet::Type.type(:lxc_interface).provider(:interface), 'basic interfac
       @provider.send(:exists?).should == false
     end
     it 'should return true when the interface exists' do
-      @provider.container.stubs(:keys).returns(['name','hwaddr','ipv4','ipv6'])
+      @provider.container.stubs(:keys).returns(['name','hwaddr','ipv4','link','type'])
       @provider.send(:exists?).should == true
     end
   end
+
   describe '#create' do
     it 'should return true when settings are applied successfully' do
       @provider.container.stubs(:save_config)
@@ -34,6 +35,125 @@ describe Puppet::Type.type(:lxc_interface).provider(:interface), 'basic interfac
     it 'should return false when LXC::Error is raised' do
       @provider.container.stubs(:set_config_item).raises(LXC::Error)
       @provider.send(:create).should == false
+    end
+  end
+
+  describe '#destroy' do
+    it 'should return true after successfully removing all items' do
+      @provider.container.stubs(:keys).returns(['name','hwaddr','ipv4','link','type'])
+      @provider.container.stubs(:clear_config_item)
+      @provider.container.stubs(:save_config)
+      @provider.send(:destroy).should == true
+    end
+    it 'should return false when LXC::Error is raised' do
+      @provider.container.stubs(:keys).returns(['name','hwaddr','ipv4','link','type'])
+      @provider.container.stubs(:clear_config_item).raises(LXC::Error)
+      @provider.send(:destroy).should == false
+    end
+  end
+
+  describe '#link' do
+    it 'should return lxcbr0 from the getter' do
+      @provider.container.stubs(:config_item).with('lxc.network.1.link').returns('lxcbr0')
+      @provider.send(:link).should == 'lxcbr0'
+    end
+    it 'should return true when the setter successfully changes the value' do
+      @provider.container.stubs(:set_config_item).with('lxc.network.1.link','lxcbr1')
+      @provider.send(:link=,'lxcbr1').should == true
+    end
+    it 'setter should return false if LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:link=,'lxcbr1').should == false
+    end
+  end
+
+  describe '#flags' do
+    it 'should return up from the getter' do
+      @provider.container.stubs(:config_item).with('lxc.network.1.flags').returns('up')
+      @provider.send(:flags).should == 'up'
+    end
+    it 'should return true when the setter successfully changes the value' do
+      @provider.container.stubs(:set_config_item).with('lxc.network.1.flags','up')
+      @provider.send(:flags=,'up').should == true
+    end
+    it 'setter should return false if LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:flags=,'up').should == false
+    end
+  end
+
+  describe '#vlan_id' do
+    it 'should return 55 from the getter' do
+      @provider.container.stubs(:config_item).with('lxc.network.1.vlan_id').returns('55')
+      @provider.send(:vlan_id).should == '55'
+    end
+    it 'should return true when the setter successfully changes the value' do
+      @provider.container.stubs(:set_config_item).with('lxc.network.1.vlan_id','66')
+      @provider.send(:vlan_id=,'66').should == true
+    end
+    it 'setter should return false if LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:vlan_id=,'66').should == false
+    end
+  end
+
+  describe '#macvlan_mode' do
+    it 'should return vepa from the getter' do
+      @provider.container.stubs(:config_item).with('lxc.network.1.macvlan_mode').returns('vepa')
+      @provider.send(:macvlan_mode).should == 'vepa'
+    end
+    it 'should return true when the setter successfully changes the value' do
+      @provider.container.stubs(:set_config_item).with('lxc.network.1.macvlan_mode','private')
+      @provider.send(:macvlan_mode=,'private').should == true
+    end
+    it 'setter should return false if LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:macvlan_mode=,'bridge').should == false
+    end
+  end
+
+  describe '#type' do
+    it 'should return phys from the getter' do
+      @provider.container.stubs(:config_item).with('lxc.network.1.type').returns('phys')
+      @provider.send(:type).should == 'phys'
+    end
+    it 'should return true when the setter successfully changes the value' do
+      @provider.container.stubs(:set_config_item).with('lxc.network.1.type','macvlan')
+      @provider.send(:type=,'macvlan').should == true
+    end
+    it 'setter should return false if LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:type=,'bridge').should == false
+    end
+  end
+
+  describe '#ipv4' do
+    it 'should return 192.168.1.100/24 from the getter' do
+      @provider.container.stubs(:config_item).with('lxc.network.1.ipv4').returns('192.168.1.100/24')
+      @provider.send(:ipv4).should == '192.168.1.100/24'
+    end
+    it 'should return true when the setter successfully changes the value' do
+      @provider.container.stubs(:set_config_item).with('lxc.network.1.ipv4','192.168.1.101/24')
+      @provider.send(:ipv4=,'192.168.1.101/24').should == true
+    end
+    it 'setter should return false if LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:ipv4=,'192.168.1.102/24').should == false
+    end
+  end
+
+  describe '#hwaddr' do
+    it 'should return 00:de:ad:be:ef:00 from the getter' do
+      @provider.container.stubs(:config_item).with('lxc.network.1.hwaddr').returns('00:de:ad:be:ef:00')
+      @provider.send(:hwaddr).should == '00:de:ad:be:ef:00'
+    end
+    it 'should return true when the setter successfully changes the value' do
+      @provider.container.stubs(:set_config_item).with('lxc.network.1.hwaddr','01:de:ad:be:ef:01')
+      @provider.send(:hwaddr=,'01:de:ad:be:ef:01').should == true
+    end
+    it 'setter should return false if LXC::Error is raised' do
+      @provider.container.stubs(:set_config_item).raises(LXC::Error)
+      @provider.send(:hwaddr=,'02:de:ad:be:ef:02').should == false
     end
   end
 end
