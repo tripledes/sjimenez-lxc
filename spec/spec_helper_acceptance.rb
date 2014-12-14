7646da182fc73b94e8fc9fbdcaeee55368d86ea8
Sergio Jimenez <sergio.jimenez@teradata.com>
2014-12-14 20:58:46 +0100

    Tried to overcome the ruby1.8 issue on Precise.
    * Tried to get puppet installed via gems on ruby 1.9 but somehow the
      test fails, Puppet exits with code 6 so beaker complains. The catalog
      applies and the container is created. It's not clear why Puppet exits
      with failure code.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -18,10 +18,21 @@ RSpec.configure do |c|
     # Install module
     puppet_module_install(:source => proj_root, :module_name => 'lxc')
     hosts.each do |host|
-      on host, shell('wget -O /tmp/puppetlabs.deb http://apt.puppetlabs.com/puppetlabs-release-stable.deb')
-      on host, shell('dpkg -i /tmp/puppetlabs.deb')
-      on host, shell('apt-get update')
-      install_package master, 'puppet'
+
+      on host, shell('touch /etc/puppet/hiera.yaml')
+      # Using this approach with Ubuntu Precise makes the test to fail
+      # as Puppet exits with 6 although the catalog seems to be applied properly
+      if options[:HOSTS].key?:'ubuntu-server-12042-x64'
+        on host, shell('apt-get purge -y ruby ruby-dev')
+        on host, shell('apt-get install -y ruby1.9.1-dev augeas-lenses debconf-utils libaugeas-ruby1.9.1 libaugeas0 virt-what')
+        on host, shell('gem1.9.1 install puppet --bindir /usr/bin --no-rdoc --no-ri')
+      else
+        on host, shell('wget -O /tmp/puppetlabs.deb http://apt.puppetlabs.com/puppetlabs-release-stable.deb')
+        on host, shell('dpkg -i /tmp/puppetlabs.deb')
+        on host, shell('apt-get update')
+        install_package host, 'puppet'
+      end
+
       on host, puppet('module','install','puppetlabs-apt','--version','1.7.0'), { :acceptable_exit_codes => [0,1] }
     end
   end
